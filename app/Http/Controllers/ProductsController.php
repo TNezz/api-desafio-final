a44a26f7489dc87163797e8fbb38adabca696d46
TNezz <bastian.leal.b@gmail.com>
2020-01-12 21:03:07 -0300

    Update ProductsController.php

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -7,27 +7,32 @@ use App\Product;
 
 class ProductsController extends Controller
 {
+    //Definición de función mostrar todo usando la clase Product
     public function getAll(){
         $products = Product::all();
         return $products;
     }
 
+    //Definición de función agregar usando la clase Product
     public function add(Request $request){
         $product = Product::create($request->all());
         return $product;
     }
 
+    //Definición de función mostrar por id usando la clase Product 
     public function get($_id){
         $product = Product::find($_id);
         return $product;
     }
 
+    //Definición de función editar usando la clase Product
     public function edit($_id, Request $request){
         $product = $this->get($_id);
         $product -> fill($request->all())->save();
         return $product;
     }
 
+    //Definición de función borrar usando la clase Product
     public function delete($_id){
         $product = $this->get($_id);
         $product-> delete();
